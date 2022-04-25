b98f13e07015b1741cfb4fe9fd4117e6cabb909c
navroopsinghsandhu <navroops806@gmail.com>
2022-04-25 15:54:21 +1000

    backend api response for login token

--- backend/EntiretyApp/views.py
+++ backend/EntiretyApp/views.py
@@ -45,8 +45,8 @@ def userLoginApi(request):
         if(userExist) :
             break
     if(userExist) :
-        return JsonResponse("Login Successful",safe=False)
-    return JsonResponse("Username or Password is incorrect",safe=False)
+        return JsonResponse({"message" : "Login Successful", "token": username},safe=False)
+    return JsonResponse({"message" : "Username or Password is incorrect", "token" : ''},safe=False)
     
 @csrf_exempt
 def productsApi(request,id=0):
